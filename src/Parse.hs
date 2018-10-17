1a397a52181e26b3b09046f650498ef828c9bf02
metaporia <kyahnkrafft@me.com>
2018-10-16 17:41:38 -0700

    Add basic datatypes.

--- src/Parse.hs
+++ src/Parse.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GADTs #-}
 module Parse where
 
 import Prelude hiding (lookup)
@@ -31,10 +32,66 @@ import Text.Show.Pretty
 --    where a <def> is "<word> [: <meaning>]" (the brackets '[' and ']'
 --    indicate that the meaning, mapping is optional. The headword, <word>, is
 --    not.
--- □  from 
--- [r| dvs <def1>    
---         --- vs ---           
+-- □  from
+-- [r| dvs <def1>
+--         --- vs ---
 --         <def2> |] to  [DefVs]
 -- □  from "q<pgNum> " to QuotationLocation
--- □   
+-- □
 
+
+
+-- Datatypes
+
+
+
+data MediaType = Play
+               | Book
+               | ShortStory
+               | Other
+               deriving (Eq, Show)
+
+
+type Title = T.Text
+
+data Author = Author { firstName :: T.Text
+                     , lastName  :: T.Text
+                     , psuedo    :: Maybe T.Text }
+                     deriving (Eq, Show)
+
+type ISBN = T.Text
+
+-- | Q: use enum to distinguish between, e.g., aphorisms, humor, insight,
+--      etc.?
+type Quotation = T.Text
+
+-- | Represents a piece of textual media.
+data Written = Title Author MediaType (Maybe ISBN)
+  deriving (Eq, Show)
+
+-- | Represents log timestamp (likely) parsed from the following format: "hh:mm:ss λ."
+data TimeStamp = TimeStamp { hr :: Int
+                           , min :: Int
+                           , sec :: Int }
+                           deriving (Eq, Show)
+
+type Headword = T.Text
+type Meaning = T.Text
+
+newtype Def = Def (Headword, Meaning) -- ( headword, optional meaning/def)
+  deriving (Eq, Show)
+
+-- | Log entry—a.t.m. only for reading logs.
+data Entry = Defn Def
+           | Writtn Written
+           deriving (Eq, Show)
+
+data Log = Log [(TimeStamp, Entry)]
+  deriving (Eq, Show)
+
+
+
+--type PgNum = Integer
+
+
+-- ISBN for pgNumbers?

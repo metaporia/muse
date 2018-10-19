f1db1bb500942fc17a30ceaf672f72d79c4bda5d
metaporia <kyahnkrafft@me.com>
2018-10-19 15:59:08 -0700

    Add `DefQuery` parser.

--- src/Parse.hs
+++ src/Parse.hs
@@ -3,24 +3,35 @@
 {-# LANGUAGE GADTs #-}
 module Parse where
 
-import Prelude hiding (lookup, min)
+import Prelude hiding (any, lookup, min)
 
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Text.RawString.QQ
 import qualified Data.Text.Lazy as TL
-import Text.Trifecta hiding (rendered, render, Rendering, Span)
+import Text.Trifecta hiding (newline, rendered, render, Rendering, Span)
 import Control.Applicative
 import Data.Monoid ((<>))
 import Data.Bifunctor
+import Data.List (stripPrefix, isPrefixOf, intercalate)
+import Data.List.Split (splitOn)
+import Text.Trifecta.Result (Result(..))
+import qualified Text.Trifecta.Result as Tri
+import Data.Maybe (catMaybes)
 
 import Control.Monad.Except
 import Control.Monad.State.Class
 import Lib
 
-import Data.Maybe (maybe)
+import Data.Maybe (maybe, isJust)
 
 import Text.Show.Pretty
+
+toMaybe :: Tri.Result a -> Maybe a
+toMaybe (Tri.Success a) = Just a
+toMaybe (Tri.Failure a) = Nothing
+
+
 
 
 -- NB:  See ~/hs-note/src/Parse.hs for trifecta examples.
@@ -42,10 +53,18 @@ import Text.Show.Pretty
 
 
 -- Datatypes
+
 periodSep :: Parser String -> Parser [String]
 periodSep p = p `sepBy` (symbol ".")
 
+--commaSep :: Parser String -> Parser [String]
+--commaSep p = p `sepBy` (symbol ",")
+
+skipOptColon :: Parser ()
 skipOptColon = skipOptional (char ':')
+
+skipOptDot :: Parser ()
+skipOptDot = skipOptional (char '.')
 
 twoDigit :: Parser Int
 twoDigit = read <$> count 2 digit <* skipOptColon
@@ -57,10 +76,187 @@ twoDigit = read <$> count 2 digit <* skipOptColon
 
 timestamp :: Parser TimeStamp
 --timestamp = fromList <$> fmap read <$> periodSep twoDigit
-timestamp = TimeStamp <$> twoDigit <*> twoDigit <*> twoDigit  <* space <* char 'λ' <* skipOptColon
+timestamp = TimeStamp <$> twoDigit 
+                      <*> twoDigit 
+                      <*> twoDigit  
+                      <* space 
+                      <* char 'λ' 
+                      <* char '.'
+                      <* space
+
+-- | Definition parsing. The following are valid definition query forms:
+--
+--   * a comma separated query list
+--
+--     > "d word1[, ..., ]"
+--
+--      - works over multiple lines
+--      - 
+--
+--   * for inline definition of headword 
+--
+--     > "d headword : meaning" 
+--
+--   * headword comparison
+--
+--     > "dvs headword1 : meaning
+--     >      --- vs ---
+--     >      headword2 : meaning"
+defMultWords :: Parser [Def]
+defMultWords = undefined --defQueryPrefix *> 
+
+
+splitByComma = T.split (\c -> c == ',') 
+
+newline :: Parser T.Text
+newline = textSymbol "\n"
+
+defQueryPrefix :: T.Text -> T.Text
+defQueryPrefix t = case T.stripPrefix "d " t of
+                     Just t -> t
+                     Nothing -> error  "Expected defQueryPrefix \"d \""
+
+dropQueryPrefix :: String -> String
+dropQueryPrefix t = case stripPrefix "d " t of
+                     Just t -> t
+                     Nothing -> error $ "Expected defQueryPrefix \"d \"; found " ++ "\"" ++ take 4 t ++ "\""
+
+pruneQuery' :: T.Text -> [T.Text]
+pruneQuery' = splitByComma . defQueryPrefix 
+
+data DefQuery = Plural [Headword]
+              | Single Headword
+              | InlineDef Headword Meaning
+              | DefVersus Headword Meaning Headword Meaning
+              deriving (Eq, Show)
+
+testStrPlural :: String
+testStrPlural = [r| d word1, word2, hyphenated-word3 |]
+
+testStrInline0  :: String
+testStrInline0 = "d word1 : meaning1; meaning2; meaning3"
+
+testStrInline1 :: String
+testStrInline1 = [r|\
+d word1 : meaning1; meaning2; meaning3;
+  followup notes...
+
+  NB: further commentary. all text to next TimeStamp should be lumped into the 
+  meaning
+|]
+
+testLog = [r|
+08:23:30 λ. d quiescence, quiescent, quiesce
+08:24:43 λ. d vouchsafed, 
+            another-word
+08:37:26 λ. d prorated, hello, mine, yours, hypochondriacal
+            second-line
+08:38:20 λ. d elegy
+08:45:37 λ. d tumbler
+08:49:57 λ. d disport
+08:56:30 λ. d larder
+08:57:29 λ. d wainscot, 
+09:12:16 λ. d fender
+09:14:12 λ. d bleat 
+09:15:48 λ. d dissever
+09:24:04 λ. d rhapsody
+|]
+
+isIndented = isPrefixOf "           " 
+
+-- 08:58:23 λ. d dahlia : a bushy flower that has the form of a ball of
+-- 08:59:03 λ. d poppy : a red or yellow single-cupped, often cleft flower.
+-- 09:14:50 λ. d wanton : ill bred
+-- 08:48:29 λ. d loth : loathe
+-- Parse entries: from timestamp to timestamp
+
+-- group entries, where an entry in a timestamp, and any number of indented
+-- lines
+-- NB: assumes that the first
+takeEntryGroup :: [String] -> ([String], [String]) -- (rst, entrygroup)
+takeEntryGroup [] = ([], [])
+takeEntryGroup (ln:lns)  = (ln :) <$> collectIndented lns
+
+
+-- | collects next n indented lines. expects first line to be indented
+collectIndented :: [String] -> ([String], [String]) -- (rst, collected)
+collectIndented [] = ([], [])
+collectIndented (ln:lns)
+  | isIndented ln = (ln :) <$> collectIndented lns
+  | otherwise = ((ln:lns), [])
+
+
+groupEntries :: [String] -> [[String]]
+groupEntries [] = []
+groupEntries lns =
+  let (rst, grp) = takeEntryGroup lns
+   in grp : groupEntries rst
+
+-- | USE THIS to split by entry.
+-- E.g., run `logToEntryGrps testLog` to view example log
+logToEntryGrps :: String -> [(TimeStamp, [String])]
+logToEntryGrps = catMaybes 
+               . fmap parseEntry 
+               . groupEntries 
+               . filter (not.null) 
+               . lines
+
+restOfStr :: Parser String
+restOfStr = many (noneOf [])
+ 
+any :: Parser String
+any = many (noneOf [])
+ 
+
+
+--entry :: Parser (Maybe TimeStamp, String)
+--entry = (,) <$> optional timestamp <*> restOfStr
 
 skip :: Parser p -> Parser ()
 skip p = () <$ p
+
+parseEntry :: [String] -> Maybe (TimeStamp, [String])
+parseEntry [] =  Nothing
+parseEntry (ln:lns)
+  | isIndented ln = error "Entry lacks TimeStamp."
+  | otherwise = 
+    let x = parse ((,) <$> timestamp <*> restOfStr) ln
+        --x :: _
+     in toMaybe $ (fmap.fmap) (:lns) x
+                    
+
+-- the next `headwords`
+
+
+-- | Parse headwords from plural def query variant (e.g. "d word1, ...,
+--  wordN").
+headwords :: String -> [String]
+headwords = splitOn ", " . dropQueryPrefix
+
+headwords' :: Parser (TimeStamp, String)
+headwords' = (fmap.fmap) dropQueryPrefix $ (,) <$> timestamp <*> any --sepBy (many $ noneOf ",") (symbol ",")
+
+mkDefQuery :: [String] -> Maybe DefQuery
+mkDefQuery [] = Nothing
+mkDefQuery (w:[]) = Just $ Single w
+mkDefQuery xs = Just $ Plural xs
+
+toDefQuery :: Parser (Maybe DefQuery)
+toDefQuery = fmap mkDefQuery $ char 'd' -- TODO case match on prefix (d vs dvs vs read, etc..)
+                             *> space 
+                             *> sepBy (many $ noneOf ",") (symbol ",")
+
+-- WIP, current 
+parseDefQueries :: String -> [(TimeStamp, Maybe DefQuery)]
+parseDefQueries = (fmap.fmap) (join . toMaybe . parse toDefQuery . intercalate ", ") 
+                . logToEntryGrps
+
+
+v0 = "08:37:26 λ. d prorated, hello, mine, yours, hypochondriacal"
+v1 = "08:38:20 λ. d elegy"
+v2 = "09:24:04 λ. d rhapsody : meaning1; meaning2;..."
+
+
 
 data MediaType = Play
                | Book
@@ -96,10 +292,10 @@ fromList :: [Int] -> Maybe TimeStamp
 fromList (x:(x':(x'':[]))) = Just $ TimeStamp x x' x''
 fromList _ = Nothing
 
-type Headword = T.Text
-type Meaning = T.Text
+type Headword = String
+type Meaning = String
 
-newtype Def = Def (Headword, Meaning) -- ( headword, optional meaning/def)
+newtype Def = Def (Headword, Maybe Meaning) -- ( headword, optional meaning/def)
   deriving (Eq, Show)
 
 -- | Log entry—a.t.m. only for reading logs.
@@ -119,3 +315,7 @@ data Log = Log [(TimeStamp, Entry)]
 
 parse :: Parser a -> String -> Result a
 parse p = parseString p mempty 
+
+show' :: Show a => a -> IO ()
+show' =  pPrint
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Test.Hspec
+import Test.QuickCheck
+import Parse
+import Text.Trifecta.Result (Result(..))
+import qualified Text.Trifecta.Result as Tri
+
+toMaybe :: Tri.Result a -> Maybe a
+toMaybe (Tri.Success a) = Just a
+toMaybe (Tri.Failure a) = Nothing
+
+defMultWords = undefined
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  -- parser
+  describe "Timestamp parser" $ do
+    it "parse timestamp \"00:11:22 λ. \" is [00, 11, 22]" $ do
+      toMaybe (parse timestamp "00:11:22 λ. ") `shouldBe` (Just $ TimeStamp 00 11 22)
+
+  -- definition query (i.) multi word list
+  describe "Parse definiton query: multiple words" $ do
+    it "headwords \"d word1, word2, hyphenated-word3\"" $ do
+    --   \n\\[ Def (\"word1\", Nothing) \
+    --   \n\ , Def (\"word2\", Nothing) \
+    --   \n\ , Def (\"hyphenated-word3\", Nothing)\]" $ do
+         headwords "d word1, word2, hyphenated-word3"
+           `shouldBe` [ "word1"
+                      , "word2"
+                      , "hyphenated-word3"]

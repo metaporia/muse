2454cce33be37eff20cd1b10a15b203f15824cdf
metaporia <kyahnkrafft@me.com>
2018-11-05 13:00:53 -0800

    Test `logEntries` on `testlog`.

--- test/Spec.hs
+++ test/Spec.hs
@@ -24,6 +24,10 @@ main =
     describe "Parse testlog" $ do
       it "parse entries testlog" $
         example $ do (toMaybe $ parse entries testlog) `shouldBe` (Just output)
+    -- testlog with dump
+    describe "Parse testlog including dumps" $ do
+      it "parse logEntries testlog" $
+        example $ do (toMaybe $ parse logEntries testlog) `shouldBe` (Just testLogWithDumpOutput)
     -- commentary
     describe "Parse commentary entry variant 1" $ do
       it "parse entries commentTs" $
@@ -60,6 +64,66 @@ main =
           (toMaybe $ parse logEntries testDump) `shouldBe`
             (Just testDumpOutput)
 
+testLogWithDumpOutput :: [LogEntry]
+testLogWithDumpOutput =
+  [ TabTsEntry
+      ( 0
+      , TimeStamp {hr = 9, min = 55, sec = 6}
+      , Read "To the Lighthouse" "Virginia Woolf")
+  , TabTsEntry
+      ( 1
+      , TimeStamp {hr = 9, min = 55, sec = 17}
+      , Def
+          (DefVersus
+             "benignant"
+             "kind; gracious; favorable;"
+             "benign"
+             "gentle, mild, or, medically, non-threatening"))
+  , TabTsEntry
+      ( 1
+      , TimeStamp {hr = 10, min = 11, sec = 45}
+      , Def
+          (DefVersus
+             "malignant"
+             "(adj.) disposed to inflict suffering or cause distress; inimical; bent on evil."
+             "malign"
+             "(adj.) having an evil disposition; spiteful; medically trheatening; (v.) to slander; to asperse; to show hatred toward."))
+  , TabTsEntry
+      ( 1
+      , TimeStamp {hr = 10, min = 17, sec = 40}
+      , Def (Defn (Just 38) ["inimical", "traduce", "virulent"]))
+  , TabTsEntry
+      ( 1
+      , TimeStamp {hr = 10, min = 18, sec = 12}
+      , Def (Defn (Just 38) ["sublime", "lintel"]))
+  , TabTsEntry
+      ( 1
+      , TimeStamp {hr = 10, min = 24, sec = 2}
+      , Quotation
+          "There was no treachery too base for the world to commit. She knew this. No happiness lasted."
+          "In \"To the Lighthouse\", by Virginia Woolf"
+          Nothing)
+  , TabTsEntry
+      ( 1
+      , TimeStamp {hr = 10, min = 25, sec = 27}
+      , Quotation
+          "Her simplicity fathomed what clever people falsified."
+          "In \"To the Lighthouse\", by Virginia Woolf"
+          Nothing)
+  , TabTsEntry
+      ( 1
+      , TimeStamp {hr = 10, min = 28, sec = 49}
+      , Def (Defn Nothing ["plover"]))
+  , TabTsEntry
+      (1, TimeStamp {hr = 10, min = 47, sec = 59}, Def (Defn Nothing ["cosmogony"]))
+  , TabTsEntry
+      ( 1
+      , TimeStamp {hr = 10, min = 49, sec = 58}
+      , Quotation
+          "But nevertheless, the fact remained, that is was nearly impossbile to dislike anyone if one looked at them."
+          "In \"To the Lighthouse\", by Virginia Woolf"
+          (Just 38))
+  ]
 testDump :: String
 testDump = [r|
 ...

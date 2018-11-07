cdcb7d8cf23c62ab59e8a411b57dc6024fc5087b
metaporia <kyahnkrafft@me.com>
2018-11-06 17:26:51 -0800

    Add `textDiff`: sliding-window levenshtein dist.

--- src/Search.hs
+++ src/Search.hs
@@ -90,3 +90,124 @@ filterBy l u = filter (\d -> d >= l && d <= u)
 
 genEntryPred :: Input SearchResult -> (LogEntry -> Bool)
 genEntryPred (Input _ _ auth title _ _) (TabTsEntry (tab, ts, ent)) = undefined
+
+
+type Tag = String
+type TagPred = String -> Bool -- | Tag predicate.
+-- TODO
+-- Crawls a `[LogEntry]` and applies to indented/nested (depth>=1) entries a
+-- tag generated from the first-level parent.
+tagIndented :: [LogEntry] -> [(LogEntry, [Tag])]
+tagIndented = undefined
+-- TODO
+-- | Generate a tag for an entry; succeeds for `Read` variant.
+--
+--   * extract one tag from title/author attr despite variance
+genTag :: Entry -> Maybe Tag
+genTag (Read title author) = undefined
+  where
+    initials = (++ ".") . intercalate "." . fmap (take 1 . trim) . words . trim
+genTag _ = Nothing
+
+-- | Trims whitespace, slides shorter string over longer, gets each pair's
+-- levenshtein distance, returns shortest.
+-- TODO make case-insensitive
+textDiff :: T.Text -> T.Text -> Int
+textDiff a b =
+  let trim = T.unwords . T.words
+      a' = trim a
+      b' = trim b
+      la' = T.length a'
+      lb' = T.length b'
+  in if | la' == lb' -> levenshtein a' b'
+        | la' > lb' -> go lb' b' la' a'
+        | otherwise -> go la' a' lb' b'
+    -- n.b. breaks if short is longer than long. do not tamper w the above
+    -- guard
+  where
+    go :: Int -> T.Text -> Int -> T.Text -> Int
+    go ls short ll long =
+      sum $ (\w -> go' (T.length w) w ll long) <$> T.words short
+      where
+        go' ls short ll long =
+          minimum . fmap (uncurry levenshtein) $
+          zip (repeat short) $ [T.take ls $ T.drop i long | i <- [0 .. ll - ls]]
+
+
+cowards :: [T.Text]
+cowards = ["Noel Coward", "Noël Coward"]
+
+coward :: [T.Text]
+coward = ["Noël Coward"]
+
+eliots :: [T.Text]
+eliots =
+  [ "George Eliot"
+  , "George Elliot"
+  , "George Elliot (Mary Ann Evans)"
+  , "George Eliot (Mary Ann Evans)"
+  ]
+
+eliot :: [T.Text]
+eliot = ["George Eliot", "Mary Ann Evans"]
+
+dostoevskys :: [T.Text]
+dostoevskys =
+  [ "Fyodor Dostoyevsky"
+  , "Fyodor Dostoevsky"
+  , "Fyodor Dostoevsky"
+  , "Fyodor Dostoevski"
+  , "Fyodor Dostoevskij"
+  , "Fyodor Dostoevskii" 
+  , "Fyodor Dostoyevski"
+  , "Fyodor Dostoyevskij"
+  , "Fyodor Dostoevsky"
+  , "Dostoyevsky"
+  , "Dostoevski"
+  , "Dostoevskij"
+  , "Dostoevskii" 
+  , "Dostoyevsky"
+  , "Dostoyevski"
+  , "Dostoyevskij"
+  , "Dostoyevskii"
+  , "Dostoyevskii"
+  ]
+
+dostoevsky :: [T.Text]
+dostoevsky = ["Fyodor Dostoevsky"]
+
+tolstoys :: [T.Text]
+tolstoys = ["Leo Tolstoy", "Count Leo Tolstoy", "Tolstoy", "Tlostoy"]
+
+tolstoy :: [T.Text]
+tolstoy = ["Leo Tolstoy"]
+
+all' :: [T.Text]
+all' = dostoevskys ++ tolstoys ++ eliots ++ cowards
+
+rate :: T.Text -> [T.Text] -> [(Int, T.Text)]
+rate canonical = fmap $ \w -> (textDiff canonical w, w) 
+
+
+-- auto-complete
+-- Qs: 
+--
+-- * add "canonical <auth>" entry to log seeds for entity recognition via
+--   `rate`?
+-- * add entity recog. checking linter option to muse CLI; could be run on save
+--   thereby showing errors in (nearly) real-time which allows for on the spot
+--   user correction.
+--
+--   This linter would: 
+--  
+--      1. grab line at cursor
+--      2. try to extract entry w citation/attribution, a.t.m. only `Read`
+--      3. rate against last ~5 authors (take first match); otherwise lazily
+--         crawl author history in chunks of size, say, 10
+--      4. display (i) the identified author or (ii) that it is unrecognized,
+--         with a list of the top, say, 5 matches above the distance threshold.
+--
+--      tl;dr; try to identify attr under cursor, if successful display result,
+--      if not show top matches (above satisfaction threshold)
+
+

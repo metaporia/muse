88413059e83e685c1d53da03cd9f568c4e277e0b
metaporia <kyahnkrafft@me.com>
2018-11-14 10:34:11 -0800

    Add "SearchSpec" test suite.

--- test/ParseSpec.hs
+++ test/ParseSpec.hs
@@ -13,13 +13,11 @@
 -----------------------------------------------------------------------------
 module ParseSpec where
 
-import Data.List (isInfixOf)
 import Data.Time
 import Helpers
 import Parse
 import Parse.Entry
 import Prelude hiding (min)
-import Search
 import Test.Hspec
 import Text.RawString.QQ
 import qualified Text.Trifecta.Result as Tri
@@ -101,13 +99,6 @@ spec = do
         example $ do
           (toMaybe $ parse logEntries tDialogue) `shouldBe`
             (Just tDialogueOut)
-
-    describe "dialogue quote filtration" $ do
-      it "tGetDialogue" $
-        example $ do
-          tGetDialogue >>= (`shouldBe` tGetDialogueOut)
-
-
 
 tDialogue = [r|
 13:36:33 λ. phrase sine qua non
@@ -243,120 +234,6 @@ testLogWithDumpOutput' =
           (Just 38))
   ]
 
-
-tGetDialogue :: IO [LogEntry]
-tGetDialogue = do
-  i <- input (Just (isInfixOf "Woolf")) Nothing [Just isDialogue, Just isQuote]
-  return $ filterWith i tDialogueFilter
-
-tGetDialogueOut =
-  [ TabTsEntry
-      ( 0
-      , TimeStamp {hr = 9, min = 55, sec = 6}
-      , Read "To the Lighthouse" "Virginia Woolf")
-  , TabTsEntry
-      ( 1
-      , TimeStamp {hr = 10, min = 24, sec = 2}
-      , Quotation
-          "There was no treachery too base for the world to commit. She knew this. No happiness lasted."
-          "In \"To the Lighthouse\", by Virginia Woolf"
-          Nothing)
-  , TabTsEntry
-      ( 0
-      , TimeStamp {hr = 9, min = 55, sec = 6}
-      , Read "To the Lighthouse" "Virginia Woolf")
-  , TabTsEntry
-      ( 1
-      , TimeStamp {hr = 10, min = 25, sec = 27}
-      , Quotation
-          "Her simplicity fathomed what clever people falsified."
-          "In \"To the Lighthouse\", by Virginia Woolf"
-          Nothing)
-  , TabTsEntry
-      ( 0
-      , TimeStamp {hr = 8, min = 34, sec = 34}
-      , Dialogue
-          "(After ~1hr of unbridled loquacity, having mercifully dammed the torrent)\nMOM: Do you mind me telling all my favorite moments?\n\n\n(Without looking up from his guitar playing)\nDAD: No, just get it over with.\n")
-  , TabTsEntry
-      ( 0
-      , TimeStamp {hr = 10, min = 49, sec = 58}
-      , Quotation
-          "But nevertheless, the fact remained, that is was nearly impossbile to dislike anyone if one looked at them."
-          "In \"To the Lighthouse\", by Virginia Woolf"
-          (Just 38))
-  ]
-
-tDialogueFilter :: [LogEntry]
-tDialogueFilter =
-  [ TabTsEntry
-      ( 0
-      , TimeStamp {hr = 9, min = 55, sec = 6}
-      , Read "To the Lighthouse" "Virginia Woolf")
-  , TabTsEntry
-      ( 1
-      , TimeStamp {hr = 9, min = 55, sec = 17}
-      , Def
-          (DefVersus
-             "benignant"
-             "kind; gracious; favorable;"
-             "benign"
-             "gentle, mild, or, medically, non-threatening"))
-  , TabTsEntry
-      ( 1
-      , TimeStamp {hr = 10, min = 11, sec = 45}
-      , Def
-          (DefVersus
-             "malignant"
-             "(adj.) disposed to inflict suffering or cause distress; inimical; bent on evil."
-             "malign"
-             "(adj.) having an evil disposition; spiteful; medically trheatening; (v.) to slander; to asperse; to show hatred toward."))
-  , TabTsEntry
-      ( 1
-      , TimeStamp {hr = 10, min = 17, sec = 40}
-      , Def (Defn (Just 38) ["inimical", "traduce", "virulent"]))
-  , TabTsEntry
-      ( 1
-      , TimeStamp {hr = 10, min = 18, sec = 12}
-      , Def (Defn (Just 38) ["sublime", "lintel"]))
-  , TabTsEntry
-      ( 1
-      , TimeStamp {hr = 10, min = 24, sec = 2}
-      , Quotation
-          "There was no treachery too base for the world to commit. She knew this. No happiness lasted."
-          "In \"To the Lighthouse\", by Virginia Woolf"
-          Nothing)
-  , TabTsEntry
-      ( 0
-      , TimeStamp {hr = 9, min = 55, sec = 6}
-      , Read "To the Lighthouse" "Virginia Woolf")
-  , TabTsEntry
-      ( 1
-      , TimeStamp {hr = 10, min = 25, sec = 27}
-      , Quotation
-          "Her simplicity fathomed what clever people falsified."
-          "In \"To the Lighthouse\", by Virginia Woolf"
-          Nothing)
-  , TabTsEntry
-      ( 1
-      , TimeStamp {hr = 10, min = 28, sec = 49}
-      , Def (Defn Nothing ["plover"]))
-  , TabTsEntry
-      ( 1
-      , TimeStamp {hr = 10, min = 47, sec = 59}
-      , Def (Defn Nothing ["cosmogony"]))
-  , TabTsEntry
-      ( 0
-      , TimeStamp {hr = 8, min = 34, sec = 34}
-      , Dialogue
-          "(After ~1hr of unbridled loquacity, having mercifully dammed the torrent)\nMOM: Do you mind me telling all my favorite moments?\n\n\n(Without looking up from his guitar playing)\nDAD: No, just get it over with.\n")
-  , TabTsEntry
-      ( 0
-      , TimeStamp {hr = 10, min = 49, sec = 58}
-      , Quotation
-          "But nevertheless, the fact remained, that is was nearly impossbile to dislike anyone if one looked at them."
-          "In \"To the Lighthouse\", by Virginia Woolf"
-          (Just 38))
-  ]
 
 
 
---
+++ test/SearchSpec.hs
@@ -0,0 +1,151 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+ -----------------------------------------------------------------------------
+-- |
+-- Module      :  SearchSpec
+-- Copyright   :  2018 Keane Yahn-Kraft
+-- License     :  GPL-3 (see the file LICENSE)
+-- Maintainer  :  kyahnkrafft@me.com
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-- Tests search predicate application.
+-----------------------------------------------------------------------------
+module SearchSpec where
+
+import Data.List (isInfixOf)
+import Data.Time
+import Helpers
+import Parse
+import Parse.Entry
+import Prelude hiding (min)
+import Search
+import Test.Hspec
+import Text.RawString.QQ
+import qualified Text.Trifecta.Result as Tri
+import Text.Trifecta
+import Text.Show.Pretty (pPrint)
+
+--import Text.Trifecta.Result (Result(..))
+--import Test.QuickCheck
+spec :: Spec
+spec = do
+    describe "dialogue quote filtration" $ do
+      it "tGetDialogue" $
+        example $ do
+          tGetDialogue >>= (`shouldBe` tGetDialogueOut)
+
+tGetDialogue :: IO [LogEntry]
+tGetDialogue = do
+  i <- input (Just (isInfixOf "Woolf")) Nothing [Just isDialogue, Just isQuote]
+  return $ filterWith i tDialogueFilter
+
+tGetDialogueOut =
+  [ TabTsEntry
+      ( 0
+      , TimeStamp {hr = 9, min = 55, sec = 6}
+      , Read "To the Lighthouse" "Virginia Woolf")
+  , TabTsEntry
+      ( 1
+      , TimeStamp {hr = 10, min = 24, sec = 2}
+      , Quotation
+          "There was no treachery too base for the world to commit. She knew this. No happiness lasted."
+          "In \"To the Lighthouse\", by Virginia Woolf"
+          Nothing)
+  , TabTsEntry
+      ( 0
+      , TimeStamp {hr = 9, min = 55, sec = 6}
+      , Read "To the Lighthouse" "Virginia Woolf")
+  , TabTsEntry
+      ( 1
+      , TimeStamp {hr = 10, min = 25, sec = 27}
+      , Quotation
+          "Her simplicity fathomed what clever people falsified."
+          "In \"To the Lighthouse\", by Virginia Woolf"
+          Nothing)
+  , TabTsEntry
+      ( 0
+      , TimeStamp {hr = 8, min = 34, sec = 34}
+      , Dialogue
+          "(After ~1hr of unbridled loquacity, having mercifully dammed the torrent)\nMOM: Do you mind me telling all my favorite moments?\n\n\n(Without looking up from his guitar playing)\nDAD: No, just get it over with.\n")
+  , TabTsEntry
+      ( 0
+      , TimeStamp {hr = 10, min = 49, sec = 58}
+      , Quotation
+          "But nevertheless, the fact remained, that is was nearly impossbile to dislike anyone if one looked at them."
+          "In \"To the Lighthouse\", by Virginia Woolf"
+          (Just 38))
+  ]
+
+tDialogueFilter :: [LogEntry]
+tDialogueFilter =
+  [ TabTsEntry
+      ( 0
+      , TimeStamp {hr = 9, min = 55, sec = 6}
+      , Read "To the Lighthouse" "Virginia Woolf")
+  , TabTsEntry
+      ( 1
+      , TimeStamp {hr = 9, min = 55, sec = 17}
+      , Def
+          (DefVersus
+             "benignant"
+             "kind; gracious; favorable;"
+             "benign"
+             "gentle, mild, or, medically, non-threatening"))
+  , TabTsEntry
+      ( 1
+      , TimeStamp {hr = 10, min = 11, sec = 45}
+      , Def
+          (DefVersus
+             "malignant"
+             "(adj.) disposed to inflict suffering or cause distress; inimical; bent on evil."
+             "malign"
+             "(adj.) having an evil disposition; spiteful; medically trheatening; (v.) to slander; to asperse; to show hatred toward."))
+  , TabTsEntry
+      ( 1
+      , TimeStamp {hr = 10, min = 17, sec = 40}
+      , Def (Defn (Just 38) ["inimical", "traduce", "virulent"]))
+  , TabTsEntry
+      ( 1
+      , TimeStamp {hr = 10, min = 18, sec = 12}
+      , Def (Defn (Just 38) ["sublime", "lintel"]))
+  , TabTsEntry
+      ( 1
+      , TimeStamp {hr = 10, min = 24, sec = 2}
+      , Quotation
+          "There was no treachery too base for the world to commit. She knew this. No happiness lasted."
+          "In \"To the Lighthouse\", by Virginia Woolf"
+          Nothing)
+  , TabTsEntry
+      ( 0
+      , TimeStamp {hr = 9, min = 55, sec = 6}
+      , Read "To the Lighthouse" "Virginia Woolf")
+  , TabTsEntry
+      ( 1
+      , TimeStamp {hr = 10, min = 25, sec = 27}
+      , Quotation
+          "Her simplicity fathomed what clever people falsified."
+          "In \"To the Lighthouse\", by Virginia Woolf"
+          Nothing)
+  , TabTsEntry
+      ( 1
+      , TimeStamp {hr = 10, min = 28, sec = 49}
+      , Def (Defn Nothing ["plover"]))
+  , TabTsEntry
+      ( 1
+      , TimeStamp {hr = 10, min = 47, sec = 59}
+      , Def (Defn Nothing ["cosmogony"]))
+  , TabTsEntry
+      ( 0
+      , TimeStamp {hr = 8, min = 34, sec = 34}
+      , Dialogue
+          "(After ~1hr of unbridled loquacity, having mercifully dammed the torrent)\nMOM: Do you mind me telling all my favorite moments?\n\n\n(Without looking up from his guitar playing)\nDAD: No, just get it over with.\n")
+  , TabTsEntry
+      ( 0
+      , TimeStamp {hr = 10, min = 49, sec = 58}
+      , Quotation
+          "But nevertheless, the fact remained, that is was nearly impossbile to dislike anyone if one looked at them."
+          "In \"To the Lighthouse\", by Virginia Woolf"
+          (Just 38))
+  ]
+
